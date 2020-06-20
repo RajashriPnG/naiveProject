74df03418f9eecadbfb52bec3d100632862b501b
Rajashri Acharyya <Rajashri.Acharyya@gds.ey.com>
2020-06-20 21:03:41 +0530

    shfile

--- always_reject.sh
+++ always_reject.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 echo Hi
 echo "You are attempting to push to the ${GITHUB_REPO_NAME} repository which has been made read-only"
 echo "Access denied, push blocked. Please contact the repository administrator."
