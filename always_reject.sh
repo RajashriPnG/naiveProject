2fc400a57f18467b1b27af8cc461a9d73e0f1e79
RajashriAcharyya <64856720+RajashriPnG@users.noreply.github.com>
2020-06-24 23:29:37 +0530

    Update always_reject.sh

--- always_reject.sh
+++ always_reject.sh
@@ -2,6 +2,7 @@
 echo Hi
 #echo "You are attempting to push to the ${GITHUB_REPO_NAME} repository which has been made read-only"
 echo " Access denied, push blocked. Please contact the repository administrator."
+git show $GITHUB_SHA --name-only > committedFiles.log
 echo $GITHUB_SHA
 curl "https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$GITHUB_SHA" | grep -F filename | awk '{print$2}' | cut -d \, -f1 > committedFiles.log
 
