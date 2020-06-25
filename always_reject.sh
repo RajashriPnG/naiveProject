2976b8edfc3e13e4df2742fd7e90a46290c5701f
RajashriAcharyya <64856720+RajashriPnG@users.noreply.github.com>
2020-06-26 00:21:33 +0530

    Update always_reject.sh

--- always_reject.sh
+++ always_reject.sh
@@ -4,7 +4,7 @@ echo Hi
 echo " Access denied, push blocked. Please contact the repository administrator."
 git show $GITHUB_SHA --name-only 
 echo $GITHUB_SHA
-curl "https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$GITHUB_SHA" | grep -F filename | awk '{print$2}' | cut -d \, -f > committedFiles.log
+curl "https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$GITHUB_SHA" | grep -F filename | awk '{print$2}' | cut -d \, -f1 > committedFiles.log
 env
 echo $GITHUB_WORKSPACE
 
