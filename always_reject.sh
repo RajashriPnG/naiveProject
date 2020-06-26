68cb5be6684f74ae537cb6e1a4455e5847323beb
RajashriAcharyya <64856720+RajashriPnG@users.noreply.github.com>
2020-06-26 23:56:53 +0530

    Update always_reject.sh

--- always_reject.sh
+++ always_reject.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
 echo Hi
 #echo "You are attempting to push to the ${GITHUB_REPO_NAME} repository which has been made read-only"
-echo " Access denied, push blocked. Please contact the repository administrator."
-git show $GITHUB_SHA --name-only 
-echo $GITHUB_SHA
-curl "https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$GITHUB_SHA" | grep -F filename | awk '{print$2}' | cut -d \, -f1 > committedFiles.log
-env
-echo $GITHUB_WORKSPACE
+#echo " Access denied, push blocked. Please contact the repository administrator."
+#git show $GITHUB_SHA --name-only 
+#echo $GITHUB_SHA
+#curl "https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$GITHUB_SHA" | grep -F filename | awk '{print$2}' | cut -d \, -f1 > committedFiles.log
+#env
+#echo $GITHUB_WORKSPACE
 
-curl  "https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$GITHUB_SHA"
+#curl  "https://api.github.com/repos/$GITHUB_REPOSITORY/commits/$GITHUB_SHA"
 
 export t="`date '+%H%M'`"
 export d="`date --iso`"
 spin=SPFileList-$d-$t
 echo $spin
+full="partial"
+
+while getopts f o
+do	case "$o" in
+	f)	full="full" ;;
+	[?])	echo >&2 "Usage: $0 [ -c ]"; exit 1;;
+	esac
+done
 #exit 1
